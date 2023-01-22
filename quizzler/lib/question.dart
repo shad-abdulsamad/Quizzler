d0ab591f1e9d51686bf1136b4fba743c20f46186
shad-abdulsamad <shad.A.A2002@gmail.com>
2023-01-22 22:22:53 +0300

    Add files via upload

---
+++ quizzler/lib/question.dart
@@ -0,0 +1,10 @@
+// ignore: file_names
+class Question {
+  late String questionText;
+  late bool answer;
+  //constructor
+  Question(String questionText, bool answer) {
+    this.questionText = questionText;
+    this.answer = answer;
+  }
+}
